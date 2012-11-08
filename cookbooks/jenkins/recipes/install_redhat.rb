ed1af5d0716f56bd2c04f95b51d1f830c227b245
Richard Shade <rshade@rightscale.com>
2012-11-08 12:53:34 -0600

    PSST-70 first pass at jenkins

---
+++ cookbooks/jenkins/recipes/install_redhat.rb
@@ -0,0 +1,34 @@
+rightscale_marker :begin
+
+remote_file "/etc/yum.repos.d/jenkins.repo" do
+  source "http://pkg.jenkins-ci.org/redhat/jenkins.repo"
+  owner "root"
+  group "root"
+  mode "0777"
+  action :create
+end
+
+remote_file "/tmp/jenkins-ci.org.key" do
+  source "http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key"
+  owner "root"
+  group "root"
+  mode "0777"
+  action :create
+end
+
+bash "import-key" do 
+  cwd "/"
+  code <<-EOF
+  rpm --import /tmp/jenkins-ci.org.key
+  touch /tmp/jenkins-key-imported
+EOF
+  flags "-ex"
+  not_if "test -e /tmp/jenkins-key-imported"
+end
+
+yum_package "jenkins" do
+  action :install
+  flush_cache [ :before ]
+end
+
+rightscale_marker :end
